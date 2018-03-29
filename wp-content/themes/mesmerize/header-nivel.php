cb742ebec007cb08ec3318ba1de14ec8112a0a7d
wesmaffazzolli <31376480+wesmaffazzolli@users.noreply.github.com>
2018-03-29 20:59:12 -0300

    Revert "Revert "Conexão das páginas""
    This reverts commit 02dffc7cc32db579ec569f6f1ddd0f01dbc596a5.

--- wp-content/themes/mesmerize/header-nivel.php
+++ wp-content/themes/mesmerize/header-nivel.php
@@ -2,12 +2,13 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" >
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="stylesheet" type="text/css" href="wp-content/themes/mesmerize/estilo.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <?php wp_head(); ?>
+
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -19,8 +20,10 @@
 
 <div id="page" class="site">
     <div class="header-wrapper">
-        <div> 
-			<?php //get_search_form(); ?>
+        <div <?php echo mesmerize_header_background_atts(); ?>>
+            <?php do_action( 'mesmerize_before_header_background' ); ?>
+			<?php mesmerize_print_video_container(); ?>
+            <?php mesmerize_print_inner_pages_header_content(); ?>
+            <?php mesmerize_print_header_separator(); ?>
         </div>
     </div>
-
--- wp-content/themes/mesmerize/template-parts/content-nivel.php
+++ wp-content/themes/mesmerize/template-parts/content-nivel.php
@@ -1,24 +1,6 @@
-<?php /* Template name: Custom Search */   
-    get_header('custom'); 
-?>
-
     <?php $wpdb->show_errors = TRUE;
     $wpdb->suppress_errors = FALSE; 
     ?>
-
-
-
-    <div class="container-fluid config-base-barra-pesquisa">
-        <div class="row">
-            <div class="col-12">
-                <form role="search" method="get" class="config-form-barra-pesquisa" action="<?php esc_url( home_url( '/search' ) )?>">
-                    <!--<input type="search" class="config-direct-barra-pesquisa" value="<?php get_search_query() ?>" name="s" />-->
-                    <input type="search" class="config-direct-barra-pesquisa" value="<?php get_search_query() ?>" name="s" />
-                    <button type="button" class="config-botao-barra-pesquisa">Pesquisar</button>
-                </form>
-            </div>
-        </div>
-    </div>
 
     <div class="container">        
 
@@ -27,7 +9,7 @@
         $param = $_GET['s'];
 
         $cursos = $wpdb->get_results($wpdb->prepare(
-        "SELECT a.igc AS instituicao_nota, a.descr AS instituicao_nome, a.descrshort, b.descr AS campus_nome, b.endereco, b.bairro, c.descr AS cidade_nome, d.uf, f.id_curso, f.descr AS curso_nome, f.duracao, f.diploma, f.dupla_diplomacao, f.intercambio, f.enade, f.cc, f.cpc AS curso_nota, g.descr AS nivel, i.descr AS titulo, k.descr AS modalidade " .
+        "SELECT a.igc AS instituicao_nota, a.descr AS instituicao_nome, a.descrshort, b.descr AS campus_nome, b.endereco, b.bairro, c.descr AS cidade_nome, d.uf, f.descr AS curso_nome, f.duracao, f.diploma, f.dupla_diplomacao, f.intercambio, f.enade, f.cc, f.cpc AS curso_nota, g.descr AS nivel, i.descr AS titulo, k.descr AS modalidade " .
         "FROM instituicao a, campus b, cidade c, estado d, campus_curso e, curso f, nivel g, curso_titulo h, titulo i, curso_modalidade j, modalidade k " .
         "WHERE a.id_instituicao = b.id_instituicao " .
         "AND b.id_cidade = c.id_cidade " .
@@ -231,7 +213,7 @@
                             <div class="col-12">
                                 <div class="row no-gutter alinhamento">
                                     <div class="col-12">
-                                        <a href="/curso/?c_id=<?php echo $curso->id_curso;?>" class="btn btn-success fluid-size">TENHO INTERESSE</a>
+                                        <a href="#" class="btn btn-success fluid-size">TENHO INTERESSE</a>
                                     </div>
                                 </div>
                             </div>
@@ -243,5 +225,3 @@
          <?php } ?>
     </div>
 </div>
-
-<?php get_footer(); ?>
